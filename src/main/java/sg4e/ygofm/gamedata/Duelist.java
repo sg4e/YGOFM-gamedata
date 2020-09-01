50ba62e9e8bfaf993cbac9c473e7395f53d135ab
sg4e <46875746+sg4e@users.noreply.github.com>
2020-08-31 22:42:23 -0400

    Add canonical names to duelists

--- src/main/java/sg4e/ygofm/gamedata/Duelist.java
+++ src/main/java/sg4e/ygofm/gamedata/Duelist.java
@@ -66,55 +66,66 @@ public class Duelist {
     public Pool getPool(Pool.Type type) {
         return pools.get(type);
     }
+
+    @Override
+    public String toString() {
+        return getName().toString();
+    }
     
     @Getter(AccessLevel.PRIVATE)
     @AllArgsConstructor
     public static enum Name {
-        SIMON(1, 5),
-        TEANA_1(2, 5),
-        JONO_1(3, 5),
-        VILLAGER_1(4, 5),
-        VILLAGER_2(5, 5),
-        VILLAGER_3(6, 5),
-        SETO_1(7, 10),
-        HEISHIN_1(8, 20),
-        REX(9, 8),
-        WEEVIL(10, 8),
-        MAI(11, 10),
-        BANDIT_KEITH(12, 12),
-        SHADI(13, 12),
-        BAKURA(14, 14),
-        PEGASUS(15, 16),
-        ISIS(16, 16),
-        KAIBA(17, 16),
-        MAGE_SOLDIER(18, 12),
-        JONO_2(19, 10),
-        TEANA_2(20, 10),
-        OCEAN_MAGE(21, 14, true),
-        SECMETON(22, 16),
-        FOREST_MAGE(23, 14, true),
-        ANUBISIUS(24, 16),
-        MOUNTAIN_MAGE(25, 14, true),
-        ATENZA(26, 16),
-        DESERT_MAGE(27, 14, true),
-        MARTIS(28, 16),
-        MEADOW_MAGE(29, 14, true),
-        KEPURA(30, 16),
-        LABYRINTH_MAGE(31, 16),
-        SETO_2(32, 18),
-        SEBEK(33, 20),
-        NEKU(34, 20, true),
-        HEISHIN_2(35, 20),
-        SETO_3(36, 20),
-        DARKNITE(37, 20),
-        NITEMARE(38, 20),
-        DUEL_MASTER_K(39, 15);
+        SIMON(1, 5, "Simon Muran"),
+        TEANA_1(2, 5, "Teana"),
+        JONO_1(3, 5, "Jono"),
+        VILLAGER_1(4, 5, "Villager1"),
+        VILLAGER_2(5, 5, "Villager2"),
+        VILLAGER_3(6, 5, "Villager3"),
+        SETO_1(7, 10, "Seto"),
+        HEISHIN_1(8, 20, "Heishin"),
+        REX(9, 8, "Rex Raptor"),
+        WEEVIL(10, 8, "Weevil Underwood"),
+        MAI(11, 10, "Mai Valentine"),
+        BANDIT_KEITH(12, 12, "Bandit Keith"),
+        SHADI(13, 12, "Shadi"),
+        BAKURA(14, 14, "Yami Bakura"),
+        PEGASUS(15, 16, "Pegasus"),
+        ISIS(16, 16, "Isis"),
+        KAIBA(17, 16, "Kaiba"),
+        MAGE_SOLDIER(18, 12, "Mage Soldier"),
+        JONO_2(19, 10, "Jono 2nd"),
+        TEANA_2(20, 10, "Teana 2nd"),
+        OCEAN_MAGE(21, 14, "Ocean Mage", true),
+        SECMETON(22, 16, "High Mage Secmeton"),
+        FOREST_MAGE(23, 14, "Forest Mage", true),
+        ANUBISIUS(24, 16, "High Mage Anubisius"),
+        MOUNTAIN_MAGE(25, 14, "Mountain Mage", true),
+        ATENZA(26, 16, "High Mage Atenza"),
+        DESERT_MAGE(27, 14, "Desert Mage", true),
+        MARTIS(28, 16, "High Mage Martis"),
+        MEADOW_MAGE(29, 14, "Meadow Mage", true),
+        KEPURA(30, 16, "High Mage Kepura"),
+        LABYRINTH_MAGE(31, 16, "Labyrinth Mage"),
+        SETO_2(32, 18, "Seto 2nd"),
+        SEBEK(33, 20, "Guardian Sebek"),
+        NEKU(34, 20, "Guardian Neku", true),
+        HEISHIN_2(35, 20, "Heishin 2nd"),
+        SETO_3(36, 20, "Seto 3rd"),
+        DARKNITE(37, 20, "DarkNite"),
+        NITEMARE(38, 20, "Nitemare"),
+        DUEL_MASTER_K(39, 15, "Duel Master K");
         
         private final int id, handSize;
+        private final String pretty;
         private final boolean mage;
         
-        private Name(int id, int handSize) {
-            this(id, handSize, false);
+        private Name(int id, int handSize, String pretty) {
+            this(id, handSize, pretty, false);
+        }
+
+        @Override
+        public String toString() {
+            return getPretty();
         }
     }
     
